82a51c27a1dd4295c3cf5b11fa029200fc19e736
MF Trung.tq <trung.tq@mediafusion.co.jp>
2023-06-28 10:30:32 +0700

    update

--- src/Routes/Room.js
+++ src/Routes/Room.js
@@ -36,9 +36,12 @@ class Room extends Component {
       status: 'Please wait...',
 
       pc_config: {
-        "iceServers": [
+        iceServers: [
+          { urls: 'stun:stun.l.google.com:19302' },
           {
-            urls: 'stun:stun.l.google.com:19302'
+            urls: 'turn:relay1.expressturn.com:3478',
+            username: 'efTSII5PQ6XYTYGZ2J',
+            credential: 'N7zCQhVnoDYCa8oQ'
           }
         ]
       },
@@ -307,39 +310,108 @@ class Room extends Component {
     // ************************************* //
     // ************************************* //
     this.socket.on('peer-disconnected', data => {
+      try {
+        // close peer-connection with this peer
+        this.state.peerConnections[data.socketID].close()
 
-      // close peer-connection with this peer
-      this.state.peerConnections[data.socketID].close()
+        // get and stop remote audio and video tracks of the disconnected peer
+        const rVideo = this.state.remoteStreams.filter(stream => stream.id === data.socketID)
+        rVideo && this.stopTracks(rVideo[0]?.stream)
 
-      // get and stop remote audio and video tracks of the disconnected peer
-      const rVideo = this.state.remoteStreams.filter(stream => stream.id === data.socketID)
-      rVideo && this.stopTracks(rVideo[0]?.stream)
+        // filter out the disconnected peer stream
+        const remoteStreams = this.state.remoteStreams.filter(stream => stream.id !== data.socketID)
 
-      // filter out the disconnected peer stream
-      const remoteStreams = this.state.remoteStreams.filter(stream => stream.id !== data.socketID)
+        this.setState(prevState => {
+          // check if disconnected peer is the selected video and if there still connected peers, then select the first
+          const selectedVideo = prevState.selectedVideo.id === data.socketID && remoteStreams.length ? { selectedVideo: remoteStreams[0] } : null
 
-      this.setState(prevState => {
-        // check if disconnected peer is the selected video and if there still connected peers, then select the first
-        const selectedVideo = prevState.selectedVideo.id === data.socketID && remoteStreams.length ? { selectedVideo: remoteStreams[0] } : null
-
-        return {
-          // remoteStream: remoteStreams.length > 0 && remoteStreams[0].stream || null,
-          remoteStreams,
-          ...selectedVideo,
-          status: data.peerCount > 1 ? `Total Connected Peers to room ${this.state.roomId}: ${data.peerCount}` : `Waiting for other peers to connect room ${this.state.roomId}`
+          return {
+            // remoteStream: remoteStreams.length > 0 && remoteStreams[0].stream || null,
+            remoteStreams,
+            ...selectedVideo,
+            status: data.peerCount > 1 ? `Total Connected Peers to room ${this.state.roomId}: ${data.peerCount}` : `Waiting for other peers to connect room ${this.state.roomId}`
+          }
         }
+        )
       }
-      )
+      catch (e) {
+        console.error(e);
+      }
     })
 
     this.socket.on('online-peer', socketID => {
-      // console.log('connected peers ...', socketID)
+      try {
+        // console.log('connected peers ...', socketID)
 
-      // create and send offer to the peer (data.socketID)
-      // 1. Create new pc
-      this.createPeerConnection(socketID, pc => {
-        // 2. Create Offer
-        if (pc) {
+        // create and send offer to the peer (data.socketID)
+        // 1. Create new pc
+        this.createPeerConnection(socketID, pc => {
+          // 2. Create Offer
+          if (pc) {
+
+            // Send Channel
+            const handleSendChannelStatusChange = (event) => {
+              console.log('send channel status: ' + this.state.sendChannels[0].readyState)
+            }
+
+            const sendChannel = pc.createDataChannel('sendChannel')
+            sendChannel.onopen = handleSendChannelStatusChange
+            sendChannel.onclose = handleSendChannelStatusChange
+
+            this.setState(prevState => {
+              return {
+                sendChannels: [...prevState.sendChannels, sendChannel]
+              }
+            })
+
+            // Receive Channels
+            const handleReceiveMessage = (event) => {
+              const message = JSON.parse(event.data)
+              // console.log(message)
+              this.setState(prevState => {
+                return {
+                  messages: [...prevState.messages, message]
+                }
+              })
+            }
+
+            const handleReceiveChannelStatusChange = (event) => {
+              if (this.receiveChannel) {
+                console.log("receive channel's status has changed to " + this.receiveChannel.readyState);
+              }
+            }
+
+            const receiveChannelCallback = (event) => {
+              const receiveChannel = event.channel
+              receiveChannel.onmessage = handleReceiveMessage
+              receiveChannel.onopen = handleReceiveChannelStatusChange
+              receiveChannel.onclose = handleReceiveChannelStatusChange
+            }
+
+            pc.ondatachannel = receiveChannelCallback
+
+
+            pc.createOffer(this.state.sdpConstraints)
+              .then(sdp => {
+                pc.setLocalDescription(sdp)
+
+                this.sendToPeer('offer', sdp, {
+                  local: this.socket.id,
+                  remote: socketID
+                })
+              })
+          }
+        })
+      }
+      catch (e) {
+        console.error(e);
+      }
+    })
+
+    this.socket.on('offer', data => {
+      try {
+        this.createPeerConnection(data.socketID, pc => {
+          pc.addStream(this.state.localStream)
 
           // Send Channel
           const handleSendChannelStatusChange = (event) => {
@@ -382,78 +454,23 @@ class Room extends Component {
 
           pc.ondatachannel = receiveChannelCallback
 
+          pc.setRemoteDescription(new RTCSessionDescription(data.sdp)).then(() => {
+            // 2. Create Answer
+            pc.createAnswer(this.state.sdpConstraints)
+              .then(sdp => {
+                pc.setLocalDescription(sdp)
 
-          pc.createOffer(this.state.sdpConstraints)
-            .then(sdp => {
-              pc.setLocalDescription(sdp)
-
-              this.sendToPeer('offer', sdp, {
-                local: this.socket.id,
-                remote: socketID
+                this.sendToPeer('answer', sdp, {
+                  local: this.socket.id,
+                  remote: data.socketID
+                })
               })
-            })
-        }
-      })
-    })
-
-    this.socket.on('offer', data => {
-      this.createPeerConnection(data.socketID, pc => {
-        pc.addStream(this.state.localStream)
-
-        // Send Channel
-        const handleSendChannelStatusChange = (event) => {
-          console.log('send channel status: ' + this.state.sendChannels[0].readyState)
-        }
-
-        const sendChannel = pc.createDataChannel('sendChannel')
-        sendChannel.onopen = handleSendChannelStatusChange
-        sendChannel.onclose = handleSendChannelStatusChange
-
-        this.setState(prevState => {
-          return {
-            sendChannels: [...prevState.sendChannels, sendChannel]
-          }
-        })
-
-        // Receive Channels
-        const handleReceiveMessage = (event) => {
-          const message = JSON.parse(event.data)
-          // console.log(message)
-          this.setState(prevState => {
-            return {
-              messages: [...prevState.messages, message]
-            }
           })
-        }
-
-        const handleReceiveChannelStatusChange = (event) => {
-          if (this.receiveChannel) {
-            console.log("receive channel's status has changed to " + this.receiveChannel.readyState);
-          }
-        }
-
-        const receiveChannelCallback = (event) => {
-          const receiveChannel = event.channel
-          receiveChannel.onmessage = handleReceiveMessage
-          receiveChannel.onopen = handleReceiveChannelStatusChange
-          receiveChannel.onclose = handleReceiveChannelStatusChange
-        }
-
-        pc.ondatachannel = receiveChannelCallback
-
-        pc.setRemoteDescription(new RTCSessionDescription(data.sdp)).then(() => {
-          // 2. Create Answer
-          pc.createAnswer(this.state.sdpConstraints)
-            .then(sdp => {
-              pc.setLocalDescription(sdp)
-
-              this.sendToPeer('answer', sdp, {
-                local: this.socket.id,
-                remote: data.socketID
-              })
-            })
         })
-      })
+      }
+      catch (e) {
+        console.error(e);
+      }
     })
 
     this.socket.on('answer', data => {
--- src/Services/Helper/constant.js
+++ src/Services/Helper/constant.js
@@ -3,6 +3,6 @@
 // export const REST_API_URL = 'http://localhost:8080/it4995';
 // export const CHAT_SERVER_URL = 'http://localhost:8080';
 //deploy
-export const REST_API_URL = 'https://it4995-server.onrender.com/it4995';
-export const CHAT_SERVER_URL = 'https://it4995-server.onrender.com';
+export const REST_API_URL = 'https://it4995-server.online/it4995';
+export const CHAT_SERVER_URL = 'https://it4995-server.online';
 //export const REST_API_URL = API_URL;
