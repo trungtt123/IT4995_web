f99ffc633c85427ef599498b80efd9192b36a739
Trung Tran <bruceleemax111@gmail.com>
2023-06-28 17:31:27 +0700

    update

--- src/Components/videos.js
+++ src/Components/videos.js
@@ -52,7 +52,7 @@ class Videos extends Component {
             videoStyles={{
               objectFit: 'cover',
               borderRadius: 5,
-              width: '100%', height: '100%'
+              width: '100%', height: 160
             }}
           />
         ) || <div></div>
